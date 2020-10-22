2d130a39d1a86e78c3a570e7a4dce6f2675741aa
hayeon17kim <qwndhs@gmail.com>
2020-10-22 11:04:17 +0900

    feat: add clientId field in ClientApp

--- hackathon-project-client/src/main/java/com/mini/project/ClientApp.java
+++ hackathon-project-client/src/main/java/com/mini/project/ClientApp.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import com.eomcs.util.Prompt;
+import com.mini.util.Prompt;
 
 public class ClientApp {
 
   static String host;
   static int port;
+  static String clientId = "$%$";
 
   public static void main(String[] args) {
     if (args.length != 2) {
@@ -44,6 +45,10 @@ public class ClientApp {
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
+      // 메시지 이전 줄에 아이디를 보낸다.
+      // clientId.equals("$%$)가 true면 로그아웃된 상태
+      // false면 로그인된 상태
+      out.println(clientId);
       out.println(message);
       out.flush();
 
@@ -66,7 +71,7 @@ public class ClientApp {
       }
     }
   }
-
+  
   private static void receiveResponse(PrintWriter out, BufferedReader in) throws Exception {
     while (true) {
       String response = in.readLine();
