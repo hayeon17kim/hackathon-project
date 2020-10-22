da39d0e3c56389b89485ede1a7e7fbae1520a6ec
hayeon17kim <qwndhs@gmail.com>
2020-10-22 14:59:44 +0900

    fix: login

--- hackathon-project-server/src/main/java/com/mini/project/handler/MainScreenCommand.java
+++ hackathon-project-server/src/main/java/com/mini/project/handler/MainScreenCommand.java
@@ -4,22 +4,40 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import com.mini.project.domain.Member;
+import com.mini.project.domain.Todo;
 
 public class MainScreenCommand implements Command {
 
+  Member member;
+  
+  public MainScreenCommand(Member member) {
+    this.member = member;
+  }
+  
   @Override
   public void execute(PrintWriter out, BufferedReader in) {
+    out.println("============================");
+    SimpleDateFormat simpl = new SimpleDateFormat("yyyy-mm-dd                        hh:mm");
+    out.println("============================");
+    out.println("아스키아트 나오는 곳");
+    out.println("명언 랜덤으로 나오는 곳");
+    out.printf("%s님, 총 %d개의 할 일이 남았습니다.", member.getName(), member.getTodoList().size());
+    
     Date date = new Date();
     System.out.println(date);
     
-    SimpleDateFormat simpl = new SimpleDateFormat("yyyy년 mm월 dd일 hh시 mm분");
     String s = simpl.format(date);
     System.out.println("date: " + s);
   }
   
-  /*
+  public int calcHowManyTodo(List<Todo> list) {
+    return 0;
+  }
+  
+  
   public void list(Member member) {
     System.out.println(member.);
   }
-  */
 }
